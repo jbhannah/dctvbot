fd7f08fb04018f38e4cce56ba5e9ab9082bc5f65
Alex Hanna <alexander.hanna@badgerlandfinancial.com>
2015-04-07 13:33:45 -0500

    Initial bot

---
+++ main.rb
@@ -0,0 +1,25 @@
+bot = Cinch::Bot.new do
+  configure do |c|
+    # Server Info
+    c.server  = "irc.chatrealm.net"
+    c.port    = 6667
+
+    # Bot User Info
+    c.nick = "dctvbot"
+    c.user = "dctvbot"
+    c.realname = "dctvbot"
+    c.channels = ["#testinn"]
+  end
+
+  trap "SIGINT" do
+    bot.log("Caught SIGINT, quitting...", :info)
+    bot.quit
+  end
+
+  trap "SIGTERM" do
+    bot.log("Caught SIGTERM, quitting...", :info)
+    bot.quit
+  end
+end
+
+bot.start
