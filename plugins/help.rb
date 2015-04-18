08219c6ae1572cb5ebec54f99f9bbd664319c05c
Alex Hanna <tinnvec@gmail.com>
2015-04-18 12:53:38 -0400

    simplify help

--- plugins/help.rb
+++ plugins/help.rb
@@ -5,35 +5,16 @@ module Plugins
   class Help
     include Cinch::Plugin
 
-    set :help, 'help [name] - Prints information about a command (or all commands with no name specified). Responds though private message only.'
+    set :help, '!help - Prints available commands and their descriptions. Responds though private message only.'
 
-    # match /help (\w+)/, method: :with_name
-    match /help$/, method: :without_name
-    match /^help (\w+)/, use_prefix: false, react_on: :private, method: :with_name
-    match /^help$/, use_prefix: false, react_on: :private, method: :without_name
+    match /help$/
 
-    def with_name(m, query)
-      active, help_available = false
-      help = ""
-      @bot.plugins.find { |plugin|
-        if plugin.class.plugin_name == query
-          active = true
-          help = plugin.class.help
-          help_available = !help.nil?
-        end
-      }
-      if !active || !help_available
-        result = active ? 'No help available.' : 'No active plugin. Type help for list of available plugins.'
-        m.user.send(result)
-      else
-        m.user.send(help)
+    def execute(m)
+      m.user.send "Readme: https://github.com/tinnvec/dctvbot/blob/master/readme.md"
+      m.user.send "Available commands:"
+      @bot.plugins.each do |plugin|
+        m.user.send plugin.class.help unless plugin.class.help.nil?
       end
-    end
-
-    def without_name(m)
-      plugin_names = []
-      @bot.plugins.each { |plugin| plugin_names << plugin.class.plugin_name unless plugin.class.help.nil? }
-      m.user.send "Readme: https://github.com/tinnvec/dctvbot/blob/master/readme.md\nActive plugins: #{plugin_names.join(', ')}"
     end
   end
 
