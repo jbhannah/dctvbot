86454ad484aeff716b741d263cd9cde925fe9a0a
Alex Hanna <tinnvec@gmail.com>
2015-04-18 22:55:32 -0400

    simplify wolfram

--- plugins/wolfram.rb
+++ plugins/wolfram.rb
@@ -14,22 +14,16 @@ module Plugins
     end
 
     def search(query)
-      wolfram = WolframAlpha::Client.new(config[:api_id], options = { :timeout => 1 } )
-      response = wolfram.query(query)
-      result = parse_response response
+      wolfram = WolframAlpha::Client.new config[:api_id], options = { :timeout => 1 }
+      response = wolfram.query query
+      input = response["Input"] # Get the input interpretation pod.
+      result = response.find { |pod| pod.title == "Result" } # Get the result pod.
       if result
-        result
+        "#{input.subpods[0].plaintext}\n#{result.subpods[0].plaintext}"
       else
         "Sorry, I've no idea"
       end
     end
-
-    def parse_response(response)
-      pod = response.find { |pod| pod.title == "Result" }
-      pod = response.pods[1] if pod.nil?
-      pod.subpods[0].plaintext
-    end
-
   end
 
 end
