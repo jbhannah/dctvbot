f98fe981cb5d0444ce4f0fe4106eb0e49d06c1ae
Alex Hanna <tinnvec@gmail.com>
2015-04-22 19:34:29 -0400

    better official live checking

--- plugins/dctv/notifier.rb
+++ plugins/dctv/notifier.rb
@@ -11,21 +11,23 @@ module Plugins
 
       def listen(m, bot)
         statuses = dctvApiJson
-        official_check = false
-        statuses.each do |status|
-          official_check = true if status["Channel"] == "1"
+        if bot.official_live
+          official_check = false
+          statuses.each do |status|
+            official_check = true if status["Channel"] == "1"
+          end
+          bot.official_live = official_check
+          return if bot.official_live
         end
-        bot.official_live = official_check
         statuses.each do |stream|
           id = Integer(stream["StreamID"])
-          return if bot.official_live
           if stream["Channel"] != "0" && stream["Alerts"] == "true" && !bot.announced.include?(id)
             channel = stream["Channel"]
             channelLink = "http://diamondclub.tv/##{channel}"
             live = Format(:white, :red, " LIVE ")
             Channel(bot.channels[0]).send(Format(:bold, "#{stream["StreamName"]} is #{live} on Channel #{channel} - #{channelLink}"))
             bot.announced << id
-            bot.official_live = true
+            bot.official_live = true if stream["Channel"] = "1"
           elsif stream["Channel"] == "0" && bot.announced.include?(id)
             bot.announced.delete(id)
           end
