90da1ec554a958974e3a76318630765872f25e61
Alex Hanna <tinnvec@gmail.com>
2015-04-24 12:19:13 -0400

    improve hedberg quote grabbing

--- plugins/dctv/toys/mitch_hedberg.rb
+++ plugins/dctv/toys/mitch_hedberg.rb
@@ -7,7 +7,7 @@ module Plugins
       class MitchHedberg
         include Cinch::Plugin
         include Cinch::Extensions::Authentication
-        
+
         match /hedberg/
 
         def execute(msg)
@@ -15,9 +15,21 @@ module Plugins
           url = "http://en.wikiquote.org/wiki/Mitch_Hedberg"
           document = Nokogiri::HTML(open(url), nil, 'utf-8')
           quotes = document.css('li')
+          quotes = clean_non_quotes(quotes.to_a)
           quote = quotes[rand 0..quotes.length].content.strip
           msg.reply quote
         end
+
+        private
+
+          def clean_non_quotes(li_nodeset)
+            li_nodeset.each do |li|
+              if li.at_css('a') || li.content =~ /^track\s\d+/i
+                li_nodeset.delete(li)
+              end
+            end
+            return li_nodeset
+          end
       end
 
     end
