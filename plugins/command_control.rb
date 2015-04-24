30ccfc726c6166b9b7cc34523ff3fb70523d9c79
Alex Hanna <tinnvec@gmail.com>
2015-04-24 11:36:20 -0400

    refactor command control plugin

--- plugins/command_control.rb
+++ plugins/command_control.rb
@@ -10,86 +10,32 @@ module Plugins
 
     match /toyson/i, method: :enable_toys
     def enable_toys(msg)
-      unless @bot.all_commands_enabled
-        msg.user.notice "All commands are currently disabled"
-        return
-      end
-      if @bot.toys_enabled
-        msg.reply "Toys are already enabled"
-      else
-        @bot.toys_enabled = true
-        msg.reply "Toys have been enabled"
-      end
+      enable_command_set(msg, @bot.toys_enabled, "Toy commands")
     end
 
     match /toysoff/i, method: :disable_toys
     def disable_toys(msg)
-      unless @bot.all_commands_enabled
-        msg.user.notice "All commands are currently disabled"
-        return
-      end
-      if @bot.toys_enabled
-        @bot.toys_enabled = false
-        msg.reply "Toys have been disabled"
-      else
-        msg.reply "Toys are already disabled"
-      end
+      disable_command_set(msg, @bot.toys_enabled, "Toy commands")
     end
 
     match /searchon/i, method: :enable_search
     def enable_search(msg)
-      unless @bot.all_commands_enabled
-        msg.user.notice "All commands are currently disabled"
-        return
-      end
-      if @bot.search_enabled
-        msg.reply "Search commands are already enabled"
-      else
-        @bot.search_enabled = true
-        msg.reply "Search commands have been enabled"
-      end
+      enable_command_set(msg, @bot.search_enabled, "Search commands")
     end
 
     match /searchoff/i, method: :disable_search
     def disable_search(msg)
-      unless @bot.all_commands_enabled
-        msg.user.notice "All commands are currently disabled"
-        return
-      end
-      if @bot.search_enabled
-        @bot.search_enabled = false
-        msg.reply "Search commands have been disabled"
-      else
-        msg.reply "Search commands are already disabled"
-      end
+      disable_command_set(msg, @bot.search_enabled, "Search commands")
     end
 
     match /dctvon/i, method: :enable_dctv
     def enable_dctv(msg)
-      unless @bot.all_commands_enabled
-        msg.user.notice "All commands are currently disabled"
-        return
-      end
-      if @bot.dctv_commands_enabled
-        msg.reply "DCTV actions are already enabled"
-      else
-        @bot.dctv_commands_enabled = true
-        msg.reply "DCTV actions have been enabled"
-      end
+      enable_command_set(msg, @bot.dctv_commands_enabled, "DCTV commands")
     end
 
     match /dctvoff/i, method: :disable_dctv
     def disable_dctv(msg)
-      unless @bot.all_commands_enabled
-        msg.user.notice "All commands are currently disabled"
-        return
-      end
-      if @bot.dctv_commands_enabled
-        @bot.dctv_commands_enabled = false
-        msg.reply "DCTV actions have been disabled"
-      else
-        msg.reply "DCTV actions are already disabled"
-      end
+      disable_command_set(msg, @bot.dctv_commands_enabled, "DCTV commands")
     end
 
     match /lockdown$/i, method: :lockdown
@@ -117,6 +63,34 @@ module Plugins
         msg.reply "All commands enabled"
       end
     end
+
+    private
+
+      def disable_command_set(msg, command_set_boolean, command_set_name)
+        unless @bot.all_commands_enabled
+          msg.user.notice "All commands are currently disabled"
+          return
+        end
+        if command_set_boolean
+          command_set_boolean = false
+          msg.reply "#{command_set_name} have been disabled"
+        else
+          msg.reply "#{command_set_name} are already disabled"
+        end
+      end
+
+      def enable_command_set(msg, command_set_boolean, command_set_name)
+        unless @bot.all_commands_enabled
+          msg.user.notice "All commands are currently disabled"
+          return
+        end
+        if command_set_boolean
+          msg.reply "#{command_set_name} are already enabled"
+        else
+          command_set_boolean = true
+          msg.reply "#{command_set_name} have been enabled"
+        end
+      end
   end
 
 end
