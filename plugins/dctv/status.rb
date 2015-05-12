6f680d0dc7bfe852a0d841aee265e5a5719e6f51
Alex Hanna <alexander.hanna@badgerlandfinancial.com>
2015-05-12 10:41:37 -0500

    Use notice and pms more correctly

--- plugins/dctv/status.rb
+++ plugins/dctv/status.rb
@@ -22,14 +22,12 @@ HELP
         onCount = 0
         apiResult.each do |result|
           unless result["Channel"] == "0"
-            # msg.reply
-            msg.user.send "#{result["StreamName"]} is live on Channel #{result["Channel"]} - http://diamondclub.tv/##{result["Channel"]}"
+            msg.user.notice "#{result["StreamName"]} is live on Channel #{result["Channel"]} - http://diamondclub.tv/##{result["Channel"]}"
             onCount += 1
           end
         end
         if onCount == 0
-          # msg.reply
-          msg.user.send "Nothing is currently live"
+          msg.user.notice "Nothing is currently live"
         end
       end
 
@@ -43,8 +41,7 @@ HELP
           entry = entries[0]
         end
         title = CGI.unescape_html entry["title"]
-        # msg.reply
-        msg.user.send "Next scheduled show: #{title} (#{timeUntil(entry["time"])})"
+        msg.reply "Next scheduled show: #{title} (#{timeUntil(entry["time"])})"
 
       end
 
@@ -52,12 +49,10 @@ HELP
       def schedule(msg)
         return unless (@bot.dctv_commands_enabled || authenticated?(msg))
         entries = getCalendarEntries
-        # msg.reply
         msg.user.send "Here are the scheduled shows for the next 48 hours:"
         entries.each do |entry|
           if entry["time"] - 48.hours < Time.new
             title = CGI.unescape_html entry["title"]
-            # msg.reply
             msg.user.send "#{title} - #{timeIsLink(entry["time"], true)}"
           end
         end
