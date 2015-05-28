d14f47e688bf66e58bf03cff847a08e1d7ca66c0
Alasdair Smith <ali@alasdairsmith.co.uk>
2015-05-28 21:40:41 +0100

    Clear second screen record list when enabled/disabled

--- plugins/dctv/secondscreenrec.rb
+++ plugins/dctv/secondscreenrec.rb
@@ -12,6 +12,7 @@ module Plugins
       match /secsrec on/, method: :on
       def on(m)
         @bot.record_second_screen = true
+        @bot.recorded_second_screen_list.clear
 
         m.user.notice "Second Screen Recording enabled"
       end
@@ -19,6 +20,7 @@ module Plugins
       match /secsrec off/, method: :off
       def off(m)
         @bot.record_second_screen = false
+        @bot.recorded_second_screen_list.clear
 
         m.user.notice "Second Screen Recording disabled"
       end
