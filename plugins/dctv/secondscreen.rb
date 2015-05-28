4ab425f07e19a6cd8ce39f741e3201470aaa54cd
Alasdair Smith <ali@alasdairsmith.co.uk>
2015-05-28 22:03:33 +0100

    When executing second screen command, check if turning off and disable second screen recording

--- plugins/dctv/secondscreen.rb
+++ plugins/dctv/secondscreen.rb
@@ -12,6 +12,13 @@ module Plugins
       match /secs (.+)/
 
       def execute(m, url)
+        if url == "off" && @bot.record_second_screen
+          # Disable second screen recording
+          @bot.record_second_screen = false
+          @bot.recorded_second_screen_list.clear
+          return
+        end
+
         if @bot.record_second_screen
           @bot.recorded_second_screen_list.push(url)
         end
