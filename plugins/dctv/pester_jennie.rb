73947600ae006642122254073c6fe584ad1af58d
Alex Hanna <alexander.hanna@badgerlandfinancial.com>
2015-05-29 09:43:44 -0500

    Finally working pester function, hard coded specifics, currently

--- plugins/dctv/pester_jennie.rb
+++ plugins/dctv/pester_jennie.rb
@@ -8,21 +8,25 @@ module Plugins
 
       listen_to :join
       def listen(m)
+        current_time = Time.now.in_time_zone('US/Pacific')
         return unless m.user.nick == "jenniej23"
         @message_timer = Timer(60) do
-          current_time = Time.now.in_time_zone('US/Pacific')
-          break unless current_time.hour > 12 && current_time.hour < 3
+          if (current_time.hour <= 11 || current_time.hour >= 2) && @message_timer.started
+            @message_timer.stop
+            next
+          end
+          next unless current_time.hour >= 12 && current_time.hour <= 2
           m.user.send "Have you tweeted about DTNS and given beatmaster the links?"
         end
+        @message_timer.start
       end
 
       match /got it/, use_prefix: false, react_on: :private
       def execute(m)
-        return unless m.user.nick == "jenniej23"
+        return unless m.user.nick == "jenniej23" && @message_timer.started
         @message_timer.stop
         m.user.send "Awesome, have a great show!"
       end
-
     end
 
   end
