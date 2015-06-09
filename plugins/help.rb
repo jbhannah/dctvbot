93824aa2dfba0be2a7be5036cd420510b332dd5a
Alex Hanna <tinnvec@gmail.com>
2015-06-09 15:01:52 -0500

    allow help to react on whisper

--- plugins/help.rb
+++ plugins/help.rb
@@ -6,6 +6,7 @@ module Plugins
     include Cinch::Plugin
 
     match /help/
+    match /help/, use_prefix: false, react_on: :private
 
     def execute(m)
       m.user.send "Readme: https://github.com/tinnvec/dctvbot/blob/master/readme.md"
