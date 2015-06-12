28d11f5fd6210fe0dbc86fd8f81fc0645d5256dd
Alex Hanna <alexander.hanna@badgerlandfinancial.com>
2015-06-12 12:42:48 -0500

    Fix for hedberg returning non quotes, now reads from different place and parses more correctly

--- plugins/dctv/toys/mitch_hedberg.rb
+++ plugins/dctv/toys/mitch_hedberg.rb
@@ -11,25 +11,41 @@ module Plugins
         match /hedberg/, group: :toys
 
         def execute(m)
-          return unless (@bot.toys_enabled || authenticated?(m))
-          url = "http://en.wikiquote.org/wiki/Mitch_Hedberg"
+          url = "http://www.buzzfeed.com/mrloganrhoades/a-complete-ranking-of-almost-every-single-mitch-hedberg-joke"
           document = Nokogiri::HTML(open(url), nil, 'utf-8')
-          quotes = document.css('li')
-          quotes = clean_non_quotes(quotes.to_a)
-          quote = quotes[rand 0..quotes.length].content.strip
+          potential_quotes = document.css('.buzz_superlist_item_wide p')
+          quotes = Array.new
+          potential_quotes.each do |quote|
+            if quote.text =~ /^\d/
+              quote = quote.inner_html.gsub(/^\d{1,3}\.\s/, '')
+              quote = quote.gsub(/\[<a href="(.+)">Listen<\/a>\]$/) { |s| $1 }
+              quotes << quote.strip
+            end
+          end
+          quote = CGI.unescape_html(quotes[rand 0..quotes.length])
           m.reply quote
         end
 
-        private
-
-          def clean_non_quotes(li_nodeset)
-            li_nodeset.each do |li|
-              if li.at_css('a') || li.content =~ /^track\s\d+/i
-                li_nodeset.delete(li)
-              end
-            end
-            return li_nodeset
-          end
+        # def execute(m)
+        #   return unless (@bot.toys_enabled || authenticated?(m))
+        #   url = "http://en.wikiquote.org/wiki/Mitch_Hedberg"
+        #   document = Nokogiri::HTML(open(url), nil, 'utf-8')
+        #   quotes = document.css('li')
+        #   quotes = clean_non_quotes(quotes.to_a)
+        #   quote = quotes[rand 0..quotes.length].content.strip
+        #   m.reply quote
+        # end
+        #
+        # private
+        #
+        #   def clean_non_quotes(li_nodeset)
+        #     li_nodeset.each do |li|
+        #       if li.at_css('a') || li.content =~ /^track\s\d+/i
+        #         li_nodeset.delete(li)
+        #       end
+        #     end
+        #     return li_nodeset
+        #   end
       end
 
     end
